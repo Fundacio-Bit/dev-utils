42764c109bcae208af92c72eaedc6164ceca7e22
gdeignacio-fundaciobit <gdeignacio@fundaciobit.org>
2025-07-18 15:36:21 +0200

    Configuracio https, elytron i secure deployment #2

---
+++ builds/wildfly-dist/docker-entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+CERT_DIR="/opt/jboss/wildfly/standalone/configuration/sslcacerts"
+KEYSTORE="$JAVA_HOME/lib/security/cacerts"
+STOREPASS="changeit"
+
+echo "Importando certificados de $CERT_DIR en el keystore Java..."
+
+for certfile in "$CERT_DIR"/*.crt; do
+  [ -e "$certfile" ] || continue  # Si no hay .crt, saltar
+  aliasname=$(basename "$certfile" .crt | tr '[:upper:]' '[:lower:]' | tr ' ' '-')
+  
+  if keytool -list -keystore "$KEYSTORE" -storepass "$STOREPASS" -alias "$aliasname" >/dev/null 2>&1; then
+    echo "El certificado '$aliasname' ya está importado, saltando..."
+  else
+    echo "Importando certificado '$aliasname'..."
+    keytool -importcert -noprompt -alias "$aliasname" -file "$certfile" -keystore "$KEYSTORE" -storepass "$STOREPASS"
+    echo "Certificado '$aliasname' importado correctamente."
+  fi
+done
+
+echo "Arrancando WildFly..."
+exec /opt/jboss/wildfly/bin/standalone.sh -Djavax.net.ssl.trustore=$KEYSTORE -Djavax.net.ssl.trustorepass=$STOREPASS -b 0.0.0.0 -bmanagement 0.0.0.0
